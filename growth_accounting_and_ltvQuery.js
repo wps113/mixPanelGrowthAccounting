10c668aeea8415a52cf3fa33a95e2ee40df85066
Will <wstern13@stanford.edu>
2016-07-22 14:30:44 -0700

    Update growth_accounting_and_ltvQuery.js

--- growth_accounting_and_ltvQuery.js
+++ growth_accounting_and_ltvQuery.js
@@ -1,5 +1,5 @@
 var params = {
-  eventToMeasure: "App Open",
+  eventToMeasure: "App Open", //This is where you'd specify event/data type you want to track
   first_date_of_valid_data: '2015-10-01' //Please make this the first valid week of your data. Often your first week of MP data is janky due to testing etc.
   from_date: '2016-05-01', //the date you want to measure from
   to_date: '2016-07-21' //the date you want to measure to
@@ -10,7 +10,7 @@ function main() {
     Events({
       from_date: params.first_date_of_valid_data,
       to_date:  params.to_date,
-      event_selectors: [{event: params.eventToMeasure}] //This is where you'd specify event/data type you want to track
+      event_selectors: [{event: params.eventToMeasure}] 
     }),
     People()
   )
